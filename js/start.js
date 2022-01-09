bec09781d98a39e74ae31bdbd82f5ba60a4543bd
hamade-kj <hamadekj15@gmail.com>
2022-01-09 20:30:54 +0100

    22

---
+++ js/start.js
@@ -0,0 +1,8 @@
+function submitForm(e) {
+    e.preventDefault();
+    let name = document.getElementById("nom").value;
+  
+    sessionStorage.setItem("name", name);
+  
+    location.href="quizz page.html"
+  }
