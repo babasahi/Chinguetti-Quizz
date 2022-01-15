decc6de47022adb8658b45962c3d6cad0a7b7e91
hamade-kj <hamadekj15@gmail.com>
2022-01-15 18:04:01 +0100

    22

--- js/userInfo.js
+++ js/userInfo.js
@@ -1,6 +1,6 @@
 
 let user_name = sessionStorage.getItem("name");
 let user_points = sessionStorage.getItem("points");
-let user_time = sessionStorage.getItem("time");
+
 document.getElementById("name").innerHTML = user_name;
 document.getElementById("points").innerHTML = user_points;
