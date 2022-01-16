8236914a773914157b9e1ee124e21f59c02047f0
hamade-kj <hamadekj15@gmail.com>
2022-01-16 13:11:29 +0100

    22

--- js/userInfo.js
+++ js/userInfo.js
@@ -2,7 +2,9 @@
 let user_name = sessionStorage.getItem("name");
 let user_points = sessionStorage.getItem("points");
 let user_noncheck = sessionStorage.getItem("nocheck");
+let user_totaleqsm = sessionStorage.getItem("TotalQeustion");
 
 document.getElementById("name").innerHTML = user_name;
+document.getElementById("totaleqsm").innerHTML = user_totaleqsm ;
 document.getElementById("points").innerHTML = user_points;
 document.getElementById("Non").innerHTML = user_noncheck ;
